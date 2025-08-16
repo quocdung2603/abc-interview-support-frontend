98fc32b7541d5b027608e427bbcf20570c9e0ec5
quocdung2603 <nguyenquocdung26032003@gmail.com>
2025-08-16 17:05:41 +0700

    config: setup main source

---
+++ apps/recruiter/src/app/app.tsx
@@ -0,0 +1,13 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@abc-interview-support-frontend/recruiter" />
+    </div>
+  );
+}
+
+export default App;
